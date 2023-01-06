0751eb30751015d1b34325658d85fe037079652f
Jaehyung Doh <knuppe22@gmail.com>
2023-01-06 21:06:34 +0900

    GameManager에 characters 추가, turn을 사용해서 player와 enemy 구분 가능

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,10 +7,21 @@ public class GameManager : MonoBehaviour
     // singleton
     public static GameManager instance = null;
 
+    public Character[] characters;
+
     public int turn = 0;  // 0: 플레이어가 warrior, 1: 플레이어가 monster
     public float turnTime = 30.0f;
     public float currentTurnTime = 30.0f;
     public int score;
+
+    public Character Player
+    {
+        get { return instance.characters[turn]; }
+    }
+    public Character Enemy
+    {
+        get { return instance.characters[1 - turn]; }
+    }
 
     private void Awake()
     {
