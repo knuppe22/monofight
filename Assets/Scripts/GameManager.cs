4207b6385a9ea2baba689ae04404417347a7ebd7
Jaehyung Doh <knuppe22@gmail.com>
2023-01-06 20:41:52 +0900

    GameManager 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // singleton
+    public static GameManager instance = null;
+
+    public int turn = 0;  // 0: 플레이어가 warrior, 1: 플레이어가 monster
+    public float turnTime = 30.0f;
+    public float currentTurnTime = 30.0f;
+    public int score;
+
+    private void Awake()
+    {
+        // singleton
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +36,15 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentTurnTime < 0)
+        {
+            turn = (turn + 1) % 2;
+            currentTurnTime = turnTime;
+        }
+    }
+
+    void GameOver()
+    {
+        // TODO
     }
 }
