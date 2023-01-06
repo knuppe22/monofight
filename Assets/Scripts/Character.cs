6233902f94c8891c43b49486d33e3f460a4aecfc
devingryu <devingryu@korea.ac.kr>
2023-01-06 21:35:48 +0900

    Character, Weapon base

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField]
+    private bool isWarrior {get;} // 전사인지, 몬스터인지 (inspector)
+    public int CharacterID => isWarrior ? 0 : 1;
+    
+    private Weapon weapon;
+    public Weapon Weapon {
+        get => weapon;
+        set {
+            if(!isWarrior) return;
+            weapon = value;
+            // TODO: enable weapon GameObject
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public int Health {get; private set;} = 100;
+    
+    public void StartAttack()
     {
-        
+        // TODO: 애니메이션 시작
+    }
+    public void OnHit(int damage)
+    {
+        // TODO: 현재 체력에 맞춰 점수 획득
+        Health -= damage;
+    }
+    public void DamageEnemy() {
+        var enemy = GameManager.instance.characters[1 - CharacterID];
+        enemy.OnHit(weapon.Damage);
     }
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
+public abstract class Weapon
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public abstract string Name {get;}
+    public abstract int Damage {get;}
 }
