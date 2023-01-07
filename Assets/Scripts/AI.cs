125368e7f42b593535d0527c3d646052007584ae
Jaehyung Doh <knuppe22@gmail.com>
2023-01-07 16:06:30 +0900

    AI WIP

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -3,22 +3,85 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+public enum AIState
+{
+    Idle,
+    Trace,
+    Attak,
+    Die
+}
+
 public class AI : MonoBehaviour
 {
+    [SerializeField]
+    private AIState state = AIState.Idle;
+    [SerializeField]
+    private float attackDistance;
+
+    private Character player;
+    private Character enemy;
+
+    void Start()
+    {
+        for (int i = 0; i < GameManager.Instance.characters.Length; i++)
+        {
+            Character character = GameManager.Instance.characters[i];
+            NavMeshAgent characterAgent = character.GetComponent<NavMeshAgent>();
+            characterAgent.speed = character.moveSpeed;
+            //characterAgent.stoppingDistance = attackDistance;
+        }
+    }
+
     void Update()
     {
-        Transform enemy = GameManager.Instance.Enemy.transform;
-        Transform player = GameManager.Instance.Player.transform;
+        
+    }
+
+    void OnTraceEnter()
+    {
+        Character player = GameManager.Instance.Player;
+        Character enemy = GameManager.Instance.Enemy;
 
         NavMeshAgent enemyNavmesh = enemy.GetComponent<NavMeshAgent>();
         NavMeshAgent playerNavmesh = player.GetComponent<NavMeshAgent>();
 
         Animator enemyAnimator = enemy.GetComponent<Animator>();
-        enemyAnimator.SetBool("IsWalking", true);
+        enemy.IsWalking = true;
 
         enemyNavmesh.isStopped = false;
         playerNavmesh.isStopped = true;
 
-        enemyNavmesh.destination = player.position;
+        enemyNavmesh.destination = player.transform.position;
+
+        StartCoroutine("OnTrace");
+    }
+
+    IEnumerator OnTrace()
+    {
+        if (true) // 거리가 얼마보다 크면
+        {
+            yield return null;
+        }
+
+        // 거리가 얼마 이하이면
+        state = AIState.Attak;
+        OnAttackEnter();
+    }
+
+    void OnAttackEnter()
+    {
+        enemy.StartAttack();
+        StartCoroutine("OnAttack");
+    }
+
+    IEnumerator OnAttack()
+    {
+        if (enemy.IsHitting) // 공격중이면
+        {
+            yield return null;
+        }
+
+        state = AIState.Idle;
+        OnTraceEnter();
     }
 }
